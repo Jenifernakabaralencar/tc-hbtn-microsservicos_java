b06a24869199bd3b94ed79868ab4b69145163fac
Jenifer <jnakabar@ciandt.com>
2022-08-09 11:37:08 -0300

    adicionano task 0

---
+++ 0x04/JPA+H2_database/com/example/jpah2demo/Endereco.java
@@ -0,0 +1,80 @@
+package com.example.jpah2demo;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "enderecos")
+public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String logradouro;
+    @Column
+    private String endereco;
+    @Column
+    private int numero;
+    @Column
+    private String bairro;
+    @Column
+    private String cidade;
+    @Column
+    private String estado;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
